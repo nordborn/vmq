1130b09e32f0b580a604e6758969f86c7d889f40
vladimirb <nordborn@gmail.com>
2021-12-09 17:27:38 +0300

    1.0.1 more func docs

--- src/vmq.erl
+++ src/vmq.erl
@@ -43,38 +43,45 @@
 %%% TOPIC %%%
 
 -spec topic_new(topic()) -> ok.
+%% @doc Creates new topic
 topic_new(Topic) ->
     apply(backend_module(), ?FUNCTION_NAME, [Topic]),
     ok.
 
 -spec topic_clean(topic()) -> ok.
+%% @doc Deletes all items from the topic
 topic_clean(Topic) ->
     apply(backend_module(), ?FUNCTION_NAME, [Topic]).
 
 -spec topic_delete(topic()) -> ok.
+%% @doc Deletes topic (table)
 topic_delete(Topic) ->
     apply(backend_module(), ?FUNCTION_NAME, [Topic]).
 
 -spec topic_length(topic()) -> non_neg_integer().
-%% Number of elements
+%% @doc Number of items in the topic
 topic_length(Topic) ->
     apply(backend_module(), ?FUNCTION_NAME, [Topic]).
 
 %%% ITEM %%%
 
 -spec put(topic(), data()) -> ok.
+%% @doc Adds new item
 put(Topic, Data) ->
     apply(backend_module(), ?FUNCTION_NAME, [Topic, Data]).
 
 -spec put_many(topic(), [data()]) -> ok.
+%% @doc Adds many items
 put_many(Topic, DataList) ->
     apply(backend_module(), ?FUNCTION_NAME, [Topic, DataList]).
 
 -spec consume(topic()) -> data() | '$empty'.
+%% @doc Consumes (get + delete from the topic) one item
 consume(Topic) ->
     apply(backend_module(), ?FUNCTION_NAME, [Topic]).
 
 -spec consume_all(topic()) -> [data()].
+%% @doc Consumes (get + delete from the topic) all items
 consume_all(Topic) ->
     apply(backend_module(), ?FUNCTION_NAME, [Topic]).
 
@@ -86,7 +93,7 @@ backend_module_set_default() ->
     backend_module(vmq_mnesia).
 
 -spec backend_module(atom()) -> ok.
-%% @doc Set any module as backend. See vmq_backend_behavior
+%% @doc Set any module as the backend. See vmq_backend_behavior
 backend_module(Module) ->
     persistent_term:put('__vmq_backend', Module).
 
